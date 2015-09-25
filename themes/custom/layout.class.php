20314f441ac83e7946ec500ba7f3dba63efd7736
Daku <admin@codeanimu.net>
2015-09-25 16:48:57 +0100

    minor tweaks

--- themes/custom/layout.class.php
+++ themes/custom/layout.class.php
@@ -10,45 +10,48 @@ class Layout {
 		global $config;
 
 		$contact_link = $config->get_string('contact_link');
+		$contact      = !empty($contact_link) ? "<br><a href='mailto:$contact_link'>Contact</a>" : "";
+		$debug        = get_debug_info();
 
 		$header_html = "";
 		ksort($page->html_headers);
 		foreach($page->html_headers as $line) {
 			$header_html .= "\t\t$line\n";
 		}
+		$header_html = ltrim($header_html);
 
 		$left_block_html = "";
 		$main_block_html = "";
 		$head_block_html = "";
-		$sub_block_html = "";
+		$sub_block_html  = "";
 
 		foreach($page->blocks as $block) {
 			switch($block->section) {
 				case "left":
 					$left_block_html .= $block->get_html(true);
 					break;
+
 				case "head":
 					$head_block_html .= $block->body;
 					break;
+
 				case "main":
 					$main_block_html .= $block->get_html(false);
 					break;
+
 				case "main_hide": //allow hiding certain main blocks
 					$main_block_html .= $block->get_html(true);
 					break;
+
 				case "subheading":
 					$sub_block_html .= $block->body; // $this->block_to_html($block, true);
 					break;
+
 				default:
 					print "<p>error: {$block->header} using an unknown section ({$block->section})";
 					break;
 			}
 		}
-
-		$debug = get_debug_info();
-
-		$contact = empty($contact_link) ? "" : "<br><a href='mailto:$contact_link'>Contact</a>";
-		/*$subheading = empty($page->subheading) ? "" : "<div id='subtitle'>{$page->subheading}</div>"; */
 
 		$wrapper = "";
 		if(strlen($page->heading) > 100) {
@@ -69,8 +72,12 @@ class Layout {
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<title>{$page->title}</title>
-$header_html
+
+		$header_html
 	</head>
 
 	<body>
@@ -79,19 +86,22 @@ $header_html
 			$head_block_html
 			$sub_block_html
 		</header>
+
 		<nav>
 			$left_block_html
 		</nav>
+
 		<article>
 			$flash_html
 			$main_block_html
 		</article>
+
 		<footer>
 			Images &copy; their respective owners,
 			<a href="http://code.shishnet.org/shimmie2/">Shimmie</a> &copy;
 			<a href="http://www.shishnet.org/">Shish</a> &amp;
 			<a href="https://github.com/shish/shimmie2/graphs/contributors">The Team</a>
-			2007-2014,
+			2007-2015,
 			based on the Danbooru concept.
 			$debug
 			$contact
