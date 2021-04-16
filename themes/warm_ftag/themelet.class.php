09dc5dc1e621846cc9ad2ade10497af79743b4ba
Daku <dakutree@codeanimu.net>
2021-04-16 13:17:55 +0100

    smaller dummy img

--- themes/warm_ftag/themelet.class.php
+++ themes/warm_ftag/themelet.class.php
@@ -29,13 +29,13 @@ class Themelet extends BaseThemelet {
 		// $base64 = base64_encode(file_get_contents($image->get_thumb_filename()));
 		return "<div class='thumb shm-thumb'>".
 		       "	<a href=\"ftag://{$basefn}\" class='{$custom_classes}' data-tags='$h_tags' data-post-id='$i_id'>".
-		       "		<img id='thumb_$i_id' title='$h_tip' alt='$h_tip' height='{$tsize[1]}' width='{$tsize[0]}' data-base64='{$h_thumb_link}' src='data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAVgBWAAD/4QBoRXhpZgAATU0AKgAAAAgABAEaAAUAAAABAAAAPgEbAAUAAAABAAAARgEoAAMAAAABAAIAAAExAAIAAAASAAAATgAAAAAAAABWAAAAAQAAAFYAAAABUGFpbnQuTkVUIHYzLjUuMTAA/9sAQwD//////////////////////////////////////////////////////////////////////////////////////9sAQwH//////////////////////////////////////////////////////////////////////////////////////8AAEQgAAQABAwEiAAIRAQMRAf/EAB8AAAEFAQEBAQEBAAAAAAAAAAABAgMEBQYHCAkKC//EALUQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+v/EAB8BAAMBAQEBAQEBAQEAAAAAAAABAgMEBQYHCAkKC//EALURAAIBAgQEAwQHBQQEAAECdwABAgMRBAUhMQYSQVEHYXETIjKBCBRCkaGxwQkjM1LwFWJy0QoWJDThJfEXGBkaJicoKSo1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoKDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uLj5OXm5+jp6vLz9PX29/j5+v/aAAwDAQACEQMRAD8AkooooA//2Q=='>".
+		       "		<img id='thumb_$i_id' title='$h_tip' alt='$h_tip' height='{$tsize[1]}' width='{$tsize[0]}' data-base64='{$h_thumb_link}' src='data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs='>".
 		       "	</a>".
 			   "	<div>".
 			   "		<a href='$h_view_link' class='thumb shm-thumb' data-tags='$h_tags' data-post-id='$i_id'>$fn</a>".
 			   "	</div>".
 		       "</div>";
-			   
+
 		// $base64 = base64_encode(file_get_contents($image->get_thumb_filename()));
 		// return "<div class='thumb shm-thumb'>".
 		       // "	<a href=\"ftag://{$fn}\" class='{$custom_classes}' data-tags='$h_tags' data-post-id='$i_id'>".
